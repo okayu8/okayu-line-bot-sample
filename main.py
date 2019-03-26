9ca3d375d1ef61cec264b4909382ce37be5a4a4a
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-03-27 01:05:26 +0900

    test

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    handle webhook body
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
