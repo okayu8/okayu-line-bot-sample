92c821be53ec725db4a935ed3b773db1fb9b9f10
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-03-27 01:01:09 +0900

    test

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print('test')
     # line_bot_api.reply_message(
     #    event.reply_token,
     #    TextSendMessage(text=event.message.text))
