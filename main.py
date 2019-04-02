1f8570d02b3a6c4a4d85979e8926b7c43f17e59e
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-04-03 00:41:35 +0900

    reply to question

--- main.py
+++ main.py
@@ -67,6 +67,8 @@ def handle_message(event):
         text = '低すぎ'
     elif event_text in keiba:
         text = str(random.choice(keiba_kekka)) + '番の馬が一着です。'
+    elif event_text[len(event.text)] = '?'
+    text = 'すみません。わかりません。'
     else:
         text = event.message.text
 
